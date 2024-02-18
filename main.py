46d7739c3a1cebe315becca9ffab88616d259559
akiracopeland <akiracopeland713@gmail.com>
2024-02-17 19:57:01 -0800

    a

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+#!/usr/bin/python3
+
 import pygame as pg
 
+from enemy import Enemy
 import constants as c
 
 pg.init()
 
-#game window
+clock = pg.time.Clock()
+
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defense")
 
-#create clock
-clock = pg.time.Clock()
+enemy_image = pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
 
-
+enemy = Enemy((200, 300), enemy_image)
 
 #game loop
 run = True
